d08b2c5e46cbb1146f640036e58e6aa01fab772a
Aan <dalang@petruknisme.com>
2022-12-08 19:10:27 +0700

    update macpkgs

--- modules/overlays/macpkgs/adguard.nix
+++ modules/overlays/macpkgs/adguard.nix
@@ -17,7 +17,7 @@ let
   }.${system} or throwSystem;
 
   sha256 = rec {
-    aarch64-darwin = "sha256-C2LMslwxLaQ2ZPuhJ8s8AeKGfpGkggAFIhVeFt7HgaI=";
+    aarch64-darwin = "sha256-tGv95fgisjbUyPaKdVYh8+fXrNcu6wDlR5qQI1kebrw=";
     x86_64-darwin = aarch64-darwin;
   }.${system} or throwSystem;
 
