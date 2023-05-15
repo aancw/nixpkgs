3cd3a299478bde7f8775b196d3d6df09bcf8d740
Aan <me@petruknisme.com>
2023-05-15 16:33:25 +0700

    Enable touch id

--- system/darwin/system.nix
+++ system/darwin/system.nix
@@ -2,5 +2,5 @@
 {
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  security.pam.enableSudoTouchIdAuth = false;
+  security.pam.enableSudoTouchIdAuth = true;
 }
